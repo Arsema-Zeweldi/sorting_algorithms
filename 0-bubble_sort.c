ea78dbaeb902b1bd855b8e18914d775c9bb649d6
Arsema-Zeweldi <arsugeb@gmail.com>
2022-07-19 05:49:00 -0700

    Task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,25 +11,24 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i;
 	size_t j;
-	int swap;
+	size_t swap;
 	int temp;
 
-	do{
+	for (i = 0; i < size; i++)
+	{
 		swap = 0;
-		for (i = 0; i < size; i++)
+		for (j = 0; j < (size - 1); j++)
 		{
-			for (j = 0; j < (size - 1); j++)
+			if (array[j] > array[j + 1])
 			{
-				if (array[j] > array[j + 1])
-				{
-					temp = array[j];
-					array[j] = array[j + 1];
-					array[j + 1] = temp;
-					swap = 1;
-					print_array(array, size);
-				}
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				swap = 1;
+				print_array(array, size);
 			}
 		}
-	} while (swap != 0);
-
+		if (!swap)
+			break;
+	}
 }
